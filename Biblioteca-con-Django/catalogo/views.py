801dbf9119226809a20ae0bc3c16af1d25dc5d9c
ivancondori2004 <144635005+ivancondori2004@users.noreply.github.com>
2023-10-19 16:08:28 -0300

    Add files via upload

---
+++ Biblioteca-con-Django/catalogo/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import CustomUserCreationForm 
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def home(request):
+    context ={}
+
+    return render(request, 'home/index.html', context)
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'registration/register.html', {'form': CustomUserCreationForm})
+    
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+            user = authenticate(
+                 username = form.cleaned_data['username'],
+                 password = form.cleaned_data['password1']
+            )
+            login(request, user)
+
+            return redirect('home')
+        else:
+
+            return render(request, 'registration/register.html', {"form": form})
+        
